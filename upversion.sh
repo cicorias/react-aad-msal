75769b722a20bf05d42a340eaf6f192dcea1793c
Shawn Cicoria <github@cicoria.com>
2018-04-26 12:36:53 -0700

    update npm version script

--- upversion.sh
+++ upversion.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # up the patch on this build...
 npm version patch
+git config --global user.email "mrrobot@nowhere.com"
+git config --global user.name "Mr Robot"
 GITPWD=$1
 URL=https://mrrobot:${GITPWD}@reactaad.visualstudio.com/_git/react-aad-msal
 echo $URL
